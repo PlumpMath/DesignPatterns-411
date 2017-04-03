aaa7bff7df384225f6af455f9e851d8b29c20dd5
Бойченко Александр <boichenko.a@aetp.ru>
2017-04-03 19:22:04 +0300

    первый коммит

---
+++ 01_FactoryMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FactoryMethod.Pattern;
+using FactoryMethod.RealCode;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pattern code:");
+            Creator[] creators = { new ConcreteCreatorA(), new ConcreteCreatorB() };
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine(product.GetType().Name);
+            }
+
+            Console.ReadKey();
+
+            //----------------------------------------------------------------//
+
+            Console.WriteLine("\nReal code:");
+            Application[] apps = { new AspNetApplication() , new JavaApplication() };         
+            foreach (Application app in apps)
+            {
+                Console.WriteLine(app.GetType().Name);
+                foreach (Technology technology in app.Technologies)
+                    Console.WriteLine("* " + technology.GetType().Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ 01_FactoryMethod/RealCode/FactoryMethod.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace FactoryMethod.RealCode
+{
+    abstract class Application
+    {
+        protected abstract void CreateApplication();
+        protected Application()
+        {
+            CreateApplication();
+        }
+        public List<Technology> Technologies { get; } = new List<Technology>();
+    }
+
+    class AspNetApplication : Application
+    {
+        protected override void CreateApplication()
+        {
+            Technologies.Add(new AspNetMvc());
+            Technologies.Add(new CSharp());
+            Technologies.Add(new SqlServer());
+        }
+    }
+
+    class JavaApplication : Application
+    {
+        protected override void CreateApplication()
+        {
+            Technologies.Add(new Java());
+            Technologies.Add(new Hibernate());
+        }
+    }
+
+    abstract class Technology
+    {
+    }
+    class AspNetMvc : Technology
+    {
+    }
+    class CSharp : Technology
+    {
+
+    }
+    class Hibernate : Technology
+    {
+
+    }
+    class Java : Technology
+    {
+
+    }
+    class SqlServer : Technology
+    {
+
+    }
+}
