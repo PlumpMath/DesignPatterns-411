239025d84712865d45f49ed051e795240d21854b
Бойченко Александр <boichenko.a@aetp.ru>
2017-04-05 19:28:07 +0300

    добавил паттерны одиночку и прототип

---
+++ Singleton/Pattern/Singleton.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton.Pattern
+{
+    class Singleton
+    {
+        private static Singleton _instance;
+        private Singleton()
+        { }
+        public static Singleton Instance()
+        {
+            return _instance ?? (_instance = new Singleton());
+        }
+    }
+}
